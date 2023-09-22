e03449d64626d4274b781f9b7f912e86d5ea188c
Kowshik <haldarkowshik@gmail.com>
2023-09-22 23:14:01 +0600

    gadd

---
+++ oma.php
@@ -0,0 +1,35 @@
+<html>
+
+
+
+<head>
+
+    <title> Home </title>
+
+</head>
+
+
+
+<body>
+
+    <h1>Hello World</h1>
+
+    <img src="../images/123.jpg" width="200px" height="100px">
+
+    <a href="http://w3schools.com">click </a>
+
+    <form action="../controller/process.php" method="Post">
+
+        username:<input type="text" name="uname">
+
+        password:<input type="password" name="pass">
+
+        <input type="submit" name="submit" value="submit">
+        <p>kheye deye kam nai!</p>
+    </form>
+
+</body>
+
+
+
+</html>
