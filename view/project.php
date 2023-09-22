886b1e1ea50a199a3ebe90eee5a0f7ce1ae401f1
Kowshik <haldarkowshik@gmail.com>
2023-09-22 22:51:01 +0600

    first commit

---
+++ view/project.php
@@ -0,0 +1,44 @@
+
+
+<!DOCTYPE html>
+
+<html>
+
+ 
+
+<head>
+
+    <title> Home </title>
+
+</head>
+
+ 
+
+<body>
+
+    <h1>Hello World</h1>
+
+    <img src="../images/123.jpg" width="200px" height="100px">
+
+    <a href="http://w3schools.com">click </a>
+
+    <form action="../controller/process.php" method="Post">
+
+        username:<input type="text" name="uname">
+
+        password:<input type="password" name="pass">
+
+        <input type="submit" name="submit" value="submit">
+    </form>
+
+ 
+
+ 
+
+ 
+
+</body>
+
+ 
+
+</html>
